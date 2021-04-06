0d389ea2b2d40394d3bbd2753fa361df39412d43
18217030 <18217030@std.stei.itb.ac.id>
2021-04-06 10:49:28 +0700

    connect view controller to custom class SavingHistoryScreen

---
+++ mogo-apps-ios/saving-history/SavingHistoryScreen.swift
@@ -0,0 +1,29 @@
+//
+//  SavingHistoryScreen.swift
+//  mogo-apps-ios
+//
+//  Created by Feby Eliana on 06/04/21.
+//
+
+import UIKit
+
+class SavingHistoryScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
