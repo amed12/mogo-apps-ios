761bff7c6fdbb05918a3c72a483ff027db790d2d
18217030 <18217030@std.stei.itb.ac.id>
2021-04-06 12:24:38 +0700

    add Transaction file

--- mogo-apps-ios/saving-history/SavingHistoryCell.swift
+++ mogo-apps-ios/saving-history/SavingHistoryCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SavingHistoryCell: UITableViewCell {
 
- 
-
+    @IBOutlet weak var typeTitle: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var amount: UILabel!
 }
---
+++ mogo-apps-ios/saving-history/Transaction.swift
@@ -0,0 +1,14 @@
+//
+//  Transaction.swift
+//  mogo-apps-ios
+//
+//  Created by Feby Eliana on 06/04/21.
+//
+
+import Foundation
+
+struct Transaction{
+    var typeTitle: String
+    var date: Date
+    var amount: Double
+}
