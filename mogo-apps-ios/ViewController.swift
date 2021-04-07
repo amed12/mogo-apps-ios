ffd6183cffded9882fd31d04043d5fcefafcf40e
paulinamaygania <paulinamaygania@gmail.com>
2021-04-08 01:34:13 +0700

    add withdraw saving done

--- mogo-apps-ios/ViewController.swift
+++ mogo-apps-ios/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addbutton: UIButton!
+    @IBOutlet weak var withdrawButton: UIButton!
+    
+    var sourceFrom = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func addaction(_ sender: UIButton) {
+        sourceFrom = sender.tag
+        showDetail(source: sender.tag)
+    }
+    
+    @IBAction func withdrawAction(_ sender: UIButton) {
+        sourceFrom = sender.tag
+        showDetail(source: sender.tag)
+    }
+    
+    func showDetail(source: Int) {
+//        sourceFrom = source
+        self.performSegue(withIdentifier: "toDetail", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? addWithdrawViewController {
+            destination.source = sourceFrom
+        }
+    }
 
 }
 
