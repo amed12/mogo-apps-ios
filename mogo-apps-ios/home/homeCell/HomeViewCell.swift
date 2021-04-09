965158580209494563fa538948174cb6a598f744
Achmad Fathullah <fathullah@qiscus.co>
2021-04-09 20:22:01 +0700

    add PR feature/home

---
+++ mogo-apps-ios/home/homeCell/HomeViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewCell.swift
+//  mogo-apps-ios
+//
+//  Created by Achmad Fathullah on 07/04/21.
+//
+
+import UIKit
+
+class HomeViewCell: UITableViewCell {
+
+    @IBOutlet weak var progressGoal: ProgressDrawer!
+    @IBOutlet weak var GoalImage: UIImageView!
+    @IBOutlet weak var GoalTitle: UILabel!
+    @IBOutlet weak var GoalDate: UILabel!
+    @IBOutlet weak var GoalBudget: UILabel!
+    @IBOutlet weak var GoalProgressView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
