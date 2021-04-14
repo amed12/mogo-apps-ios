35aadf65f03cce1aaaa72705aaee1ac0fa9de0f5
Achmad Fathullah <fathullah@qiscus.co>
2021-04-14 22:22:21 +0700

    change target date and total saved

--- mogo-apps-ios/home/HomeController.swift
+++ mogo-apps-ios/home/HomeController.swift
@@ -208,7 +208,7 @@ class HomeController :UIViewController,UITableViewDataSource,UITableViewDelegate
     }
     
     func generateDummyScenario4()  {
-        let goal1 = GoalObject(icon: "rocket", name: "Trip to Labuan Bajo", goalBudget: 5000000, targetDate: "May 5, 2022", amountSaving: 385000, totalSaving: 2000000, isComplete: false, savingFrequency: "Monthly", savingDate: "28", savingTime: "12:00")
+        let goal1 = GoalObject(icon: "rocket", name: "Trip to Labuan Bajo", goalBudget: 5000000, targetDate: "January 5, 2022", amountSaving: 385000, totalSaving: 2000000, isComplete: false, savingFrequency: "Monthly", savingDate: "28", savingTime: "12:00")
         let goal2 = GoalObject(icon: "iphone", name: "iPhone 12", goalBudget: 12000000, targetDate: "Dec 20, 2021", amountSaving: 1500000, totalSaving: 0, isComplete: false, savingFrequency: "Weekly", savingDate: "Sunday", savingTime: "18:00")
         let goal3 = GoalObject(icon: "party", name: "Sweet 17th", goalBudget: 2700000, targetDate: "Jan 1, 2022", amountSaving: 300000, totalSaving: 0, isComplete: false, savingFrequency: "Monthly", savingDate: "15", savingTime: "18:00")
         listGoal = [goal1,goal2,goal3]
@@ -217,7 +217,7 @@ class HomeController :UIViewController,UITableViewDataSource,UITableViewDelegate
     }
     
     func generateDummyScenario5()  {
-        let goal1 = GoalObject(icon: "rocket", name: "Trip to Labuan Bajo", goalBudget: 5000000, targetDate: "May 5, 2022", amountSaving: 385000, totalSaving: 1500000, isComplete: false, savingFrequency: "Monthly", savingDate: "28", savingTime: "12:00")
+        let goal1 = GoalObject(icon: "rocket", name: "Trip to Labuan Bajo", goalBudget: 5000000, targetDate: "Feb 5, 2022", amountSaving: 385000, totalSaving: 1500000, isComplete: false, savingFrequency: "Monthly", savingDate: "28", savingTime: "12:00")
         let goal2 = GoalObject(icon: "iphone", name: "iPhone 12", goalBudget: 12000000, targetDate: "Dec 20, 2021", amountSaving: 1500000, totalSaving: 0, isComplete: false, savingFrequency: "Weekly", savingDate: "Sunday", savingTime: "18:00")
         let goal3 = GoalObject(icon: "party", name: "Sweet 17th", goalBudget: 2700000, targetDate: "Jan 1, 2022", amountSaving: 300000, totalSaving: 0, isComplete: false, savingFrequency: "Monthly", savingDate: "15", savingTime: "18:00")
         listGoal = [goal1,goal2,goal3]
@@ -234,7 +234,7 @@ class HomeController :UIViewController,UITableViewDataSource,UITableViewDelegate
     }
     
     func generateDummyScenario7()  {
-        let goal1 = GoalObject(icon: "rocket", name: "Trip to Labuan Bajo", goalBudget: 5000000, targetDate: "May 5, 2022", amountSaving: 385000, totalSaving: 4000000, isComplete: false, savingFrequency: "Monthly", savingDate: "28", savingTime: "12:00")
+        let goal1 = GoalObject(icon: "rocket", name: "Trip to Labuan Bajo", goalBudget: 5000000, targetDate: "April 5, 2022", amountSaving: 385000, totalSaving: 3500000, isComplete: false, savingFrequency: "Monthly", savingDate: "28", savingTime: "12:00")
         let goal2 = GoalObject(icon: "iphone", name: "iPhone 12", goalBudget: 12000000, targetDate: "Dec 20, 2021", amountSaving: 1500000, totalSaving: 0, isComplete: false, savingFrequency: "Weekly", savingDate: "Sunday", savingTime: "18:00")
         let goal3 = GoalObject(icon: "party", name: "Sweet 17th", goalBudget: 2700000, targetDate: "Jan 1, 2022", amountSaving: 300000, totalSaving: 0, isComplete: false, savingFrequency: "Monthly", savingDate: "15", savingTime: "18:00")
         listGoal = [goal1,goal2,goal3]
